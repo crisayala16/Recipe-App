8171df7390fea3136cb5e29cdd1ea62edade6e15
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-03-28 23:59:44 -0400

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
 	var ingredients = [];
 	var ingredient;
 	var queryUrl;
-
+	var currentRecipe;
+	var currentLine;
+	var ingArray;
+	var foodType = "all";
 	$(document).on("click", "#add-ingredient", function(){
 		ingredient = $("#ingredient-input").val().trim();
 		ingredients.push(ingredient);
@@ -10,11 +13,15 @@ $(document).ready(function(){
 			+ ingredient + "</button>");
 		console.log(ingredients);
 	});
+	$(document).on("click", "#food-type-btn", function(){
+		foodType = $("#food-type-input").val().trim();
+		$("#food-type-input").val("");
+	})
 
 	$(document).on("click", "#find-recipes", function(){
 		var ingredientString = ingredients.join("+");
 		console.log(ingredientString);
-		queryUrl = "https://api.edamam.com/search?q=" + ingredientString
+		queryUrl = "https://api.edamam.com/search?q=" + foodType
 		+ "&app_id=06ea1511&app_key=bdeec150a8701cdf840910049d39fb52&from=0&to=10&calories=gte%20591,%20lte%20722"
 		$.ajax({
 			url: queryUrl,
@@ -23,15 +30,27 @@ $(document).ready(function(){
 			var results = response.hits;
 
 			for(var i = 0; i < results.length; i++){
-				var currentRecipe = results[i].recipe;
-				console.log(currentRecipe);
-				var div = $("<div id='recipe-box'>");
-				div.append("<h3 class='text-center'>" + currentRecipe.label + "</h3>")
-				div.append("<img src='" + currentRecipe.image + "'>");
-				div.append("<h4>" + currentRecipe.source + "</h4>");
-				$("#recipes").append(div);
-
+				currentRecipe = results[i].recipe;
+				for(var x = 0; x < currentRecipe.ingredientLines.length; x++){
+				currentLine = currentRecipe.ingredientLines[x];
+				ingArray = currentLine.split(" ");
+				console.log(ingArray);
 				}
+				if(ingArray.indexOf(ingredients[i]))
+				
+
+
+			}
+
+			// for(var i = 0; i < results.length; i++){
+			// 	var currentRecipe = results[i].recipe;
+			// 	console.log(currentRecipe);
+			// 	var div = $("<div id='recipe-box'>");
+			// 	div.append("<h3 class='text-center'>" + currentRecipe.label + "</h3>")
+			// 	div.append("<img src='" + currentRecipe.image + "'>");
+			// 	div.append("<h4>" + currentRecipe.source + "</h4>");
+			// 	$("#recipes").append(div);
+			// 	}
 		})
 	})
 })
