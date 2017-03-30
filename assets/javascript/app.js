b8e80de9a85885081d117c89b5e3ec078b5c0201
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-03-29 21:36:11 -0400

    updated files 3-29

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,12 @@ $(document).ready(function(){
 	var currentRecipe;
 	var currentLine;
 	var ingArray;
+	var lineItem;
+	var lowerArray = [];
+	var matchingLines = 0;
+	var lineHasIngredient;
 	var foodType = "all";
+
 	$(document).on("click", "#add-ingredient", function(){
 		ingredient = $("#ingredient-input").val().trim();
 		ingredients.push(ingredient);
@@ -15,6 +20,7 @@ $(document).ready(function(){
 	});
 	$(document).on("click", "#food-type-btn", function(){
 		foodType = $("#food-type-input").val().trim();
+		$("#food-type-area").html("<button class='btn btn-info'>" + foodType + "</button>");
 		$("#food-type-input").val("");
 	})
 
@@ -22,7 +28,7 @@ $(document).ready(function(){
 		var ingredientString = ingredients.join("+");
 		console.log(ingredientString);
 		queryUrl = "https://api.edamam.com/search?q=" + foodType
-		+ "&app_id=06ea1511&app_key=bdeec150a8701cdf840910049d39fb52&from=0&to=10&calories=gte%20591,%20lte%20722"
+		+ "&app_id=06ea1511&app_key=bdeec150a8701cdf840910049d39fb52&from=0&to=1000&calories=gte%20591,%20lte%20722"
 		$.ajax({
 			url: queryUrl,
 			method: "GET"
@@ -31,26 +37,45 @@ $(document).ready(function(){
 
 			for(var i = 0; i < results.length; i++){
 				currentRecipe = results[i].recipe;
+				matchingLines = 0;
+				lineHasIngredient = false;
+				console.log("Inside Recipe result number #" + i);
 				for(var x = 0; x < currentRecipe.ingredientLines.length; x++){
-				currentLine = currentRecipe.ingredientLines[x];
-				ingArray = currentLine.split(" ");
-				console.log(ingArray);
+					currentLine = currentRecipe.ingredientLines[x];
+					ingArray = currentLine.split(" ");
+					lowerArray = [];
+					for(var a = 0; a < ingArray.length; a++){
+						var lineItem = ingArray[a].toLowerCase();
+						lowerArray.push(lineItem.replace(/\,\(\)/g,""));
+					}
+					if(lineHasIngredient === false){
+
+						for(var y = 0; y < ingredients.length; y++){
+
+							if(lowerArray.indexOf(ingredients[y]) === -1){
+								lineHasIngredient = false;								
+							} else if(lowerArray.indexOf(ingredients[y]) === 0){
+								lineHasIngredient = true;
+								console.log(lowerArray);
+								console.log(ingredients[y]);
+								console.log("current line matches a user ingredient");
+								matchingLines++;
+								
+							}
+						}
+					}
+					console.log(matchingLines);
 				}
-				if(ingArray.indexOf(ingredients[i]))
-				
-
-
+				if(matchingLines === currentRecipe.ingredientLines.length || matchingLines > 3){
+					
+					var div = $("<div id='recipe-box'>");
+					div.append("<h3 class='text-center'>" + currentRecipe.label + "</h3>")
+					div.append("<img src='" + currentRecipe.image + "'>");
+					div.append("<h4>" + currentRecipe.source + "</h4>");
+					$("#recipes").append(div);
+					console.log("Recipe Posted!");
+				}
 			}
-
-			// for(var i = 0; i < results.length; i++){
-			// 	var currentRecipe = results[i].recipe;
-			// 	console.log(currentRecipe);
-			// 	var div = $("<div id='recipe-box'>");
-			// 	div.append("<h3 class='text-center'>" + currentRecipe.label + "</h3>")
-			// 	div.append("<img src='" + currentRecipe.image + "'>");
-			// 	div.append("<h4>" + currentRecipe.source + "</h4>");
-			// 	$("#recipes").append(div);
-			// 	}
 		})
 	})
 })
