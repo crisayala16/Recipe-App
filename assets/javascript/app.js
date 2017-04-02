3e2d48dee43ba0edf91f2ad5c66580d878475fc5
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-04-02 00:55:52 -0400

    firebase implementation 4/2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,19 @@ $(document).ready(function(){
 	var minCal = "0";
 	var maxCal = "4000";
 	var health = "";
+	var onRecipe = 0;
+	var config = {
+    apiKey: "AIzaSyCnDxa0VrOGyZCA8Z7ojYjTzYYn8P5_Lp4",
+    authDomain: "recipe-app-162617.firebaseapp.com",
+    databaseURL: "https://recipe-app-162617.firebaseio.com",
+    projectId: "recipe-app-162617",
+    storageBucket: "recipe-app-162617.appspot.com",
+    messagingSenderId: "30198600181"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
 
+//On click event to add new ingredients
 	$(document).on("click", "#add-ingredient", function(){
 		ingredient = $("#ingredient-input").val().trim();
 		ingredients.push(ingredient);
@@ -22,34 +34,40 @@ $(document).ready(function(){
 		$("#ingredient-input").val("");
 		console.log(ingredients);
 	});
+//on click event to add the food-type of the recipe search
 	$(document).on("click", "#food-type-btn", function(){
 		foodType = $("#food-type-input").val().trim();
 		$("#food-type-area").html("<button class='btn btn-info'>" + foodType + "</button>");
 		$("#food-type-input").val("");
 	});
+//On click event that launches the search query and matches recipes 
+//that contain the specified ingredients
 	$(document).on("click", "#find-recipes", function(){
+		database.ref().set({});
 		var ingredientString = ingredients.join("+");
 		console.log(ingredientString);
 		minCal = $("#min-cal").val().trim();
 		maxCal = $("#max-cal").val().trim();
-		console.log("Min cal: " + minCal);
-		console.log("Max cal: " + maxCal);
 		queryUrl = "https://api.edamam.com/search?q=" + foodType
 		+ "&app_id=06ea1511&app_key=bdeec150a8701cdf840910049d39fb52&from=0&to=100&calories=gte " + minCal +
 		",lte " + maxCal;
+		//Ajax Call
 		$.ajax({
 			url: queryUrl,
 			method: "GET"
 		}).done(function(response){
 			var results = response.hits;
-
+			//Ingredient Matching Algorithm
 			for(var i = 0; i < results.length; i++){
 				currentRecipe = results[i].recipe;
 				matchingLines = 0;
 				lineHasIngredient = false;
 				console.log("Inside Recipe result number #" + i);
+				//Looks Inside each ingredient line
 				for(var x = 0; x < currentRecipe.ingredientLines.length; x++){
+					//Prepares the user ingredients in an array
 					currentLine = currentRecipe.ingredientLines[x];
+					//Prepares each recipe's ingredient line in an array
 					ingArray = currentLine.split(" ");
 					lowerArray = [];
 					for(var a = 0; a < ingArray.length; a++){
@@ -57,7 +75,7 @@ $(document).ready(function(){
 						lowerArray.push(lineItem.replace(/\,\(\)/g,""));
 					}
 					if(lineHasIngredient === false){
-
+						//Checks If the user ingredient matches the recipe ingredient
 						for(var y = 0; y < ingredients.length; y++){
 
 							if(lowerArray.indexOf(ingredients[y]) === -1){
@@ -72,17 +90,54 @@ $(document).ready(function(){
 						}
 					}
 				}
+				//If the Ingredients Matched a certain amount of
+				//times, then post the recipe information
 				if(matchingLines === currentRecipe.ingredientLines.length || matchingLines > 0){
-					
-					var div = $("<div id='recipe-box'>");
-					div.append("<h3 class='text-center'>" + currentRecipe.label + "</h3>")
-					div.append("<img src='" + currentRecipe.image + "'>");
-					div.append("<h4>" + currentRecipe.source + "</h4>");
-					div.append("<h4>Matching Lines: " + matchingLines + "</h4>");
+					var recImage = currentRecipe.image;
+					var recLabel = currentRecipe.label;
+					var recSource = currentRecipe.source;
+					var recUrl = currentRecipe.url;
+					var fullIngredients = currentRecipe.ingredientLines;
+					var recYield = currentRecipe.yield;
+					var recCalories = Math.round(currentRecipe.calories);
+
+					var div = $("<div class='recipe-box'>");
+					div.append("<a href='recipe.html' target='_blank'><h3 class='text-center' id='recipe-label' order='" + onRecipe + "'>" + recLabel + "</h3></a>");
+					div.append("<img src='" + recImage + "'>");
+					div.append("<h4>" + recCalories + " Calories</h4>");
+					div.append("<h4>" + recSource + "</h4>");
 					$("#recipes").append(div);
 					console.log("Recipe Posted!");
+					onRecipe++;
+
+					database.ref().push({
+						image: recImage,
+						label: recLabel,
+						source: recSource,
+						url: recUrl,
+						ingredients: fullIngredients,
+						yield: recYield,
+						calories: recCalories
+					})
+
 				}
 			}
+			//End of Ingredient Matching Algorithm
 		})
+	})
+
+	$(document).on("click", "#recipe-label", function(){
+		var order = $(this).attr("order");
+		database.ref().on("value", function(snapshot){
+			var data = snapshot.val();
+			var keys = Object.keys(data);
+			var clickedRecipe = keys[order];
+			var thisLabel = data[clickedRecipe].label;
+			console.log(thisLabel);
+			$("#recipe-detail").append("<h2 class='text-center'>" + thisLabel + "</h2>");
+
+		})
+
+
 	})
 })
